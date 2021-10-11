ed2f97eaf6fd2d2ed3024675261485b4fe7584c1
jkasper <jkasper@student.42heilbronn.de>
2021-10-11 23:29:17 +0200

    Todo: itssimple.sh

---
+++ Level1/main.c
@@ -0,0 +1,9 @@
+#include "userheader.h"
+#include <stdio.h>
+
+int	main(void)
+{
+	printf("Hey you made it!\n");
+	printf("and this is from the userheader: %i\n", DEFINED_VAR1);
+}
+
---
+++ Level1/userheader.h
@@ -0,0 +1,6 @@
+#ifndef USERHEADER_H
+# define USERHEADER_H
+
+# define DEFINED_VAR1 3
+
+#endif /*USERHEADER_H*/
