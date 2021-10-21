4852d1c698d428b42ce7472768eb4fcd638b0ca1
jkasper <jkasper@student.42heilbronn.de>
2021-10-21 13:12:45 +0200

    library should work now

--- Level4/main.c
+++ Level4/main.c
@@ -1,10 +1,9 @@
-#include "./inc/usefullib.h"
 #include "someheader.h"
-//#include "usefullib.h"
+#include "usefullib.h"
 
 int main(void)
 {
-//   use_printf();
+   use_printf();
     sub_sub(1);
     return (0);
 }
